14b7d380ac40a37ee5d1177a67a70a373cc05bbf
allenliuzihao <allenliuzihao@gmail.com>
2020-09-26 17:15:07 -0700

    init commit

---
+++ RayTracingTutorial/RayTracingTutorial.cpp
@@ -0,0 +1,37 @@
+// RayTracingTutorial.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    const int image_width = 256;
+    const int image_height = 256;
+    
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int row = image_height - 1; row >= 0; --row) {
+        for (int col = 0; col < image_width; ++col) {
+            double r = double(col) / ((double)image_width - 1.0);
+            double g = double(row) / ((double)image_height - 1.0);
+            double b = 0.25;
+
+            int ir = static_cast<int>(255 * r);
+            int ig = static_cast<int>(255 * g);
+            int ib = static_cast<int>(255 * b);
+            
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
