fc464bb018d01e8fb0dfbb3cf315d7b255a2d505
allenliuzihao <allenliuzihao@gmail.com>
2020-09-26 19:45:32 -0700

    basic ray tracer

--- RayTracingTutorial/RayTracingTutorial.cpp
+++ RayTracingTutorial/RayTracingTutorial.cpp
@@ -5,23 +5,41 @@
 
 #include "vec3.h"
 #include "color.h"
+#include "ray.h"
+
+color ray_color(const ray& r) {
+    vec3 unit_dir = unit_vector(r.direction());
+    double t = 0.5 * (unit_dir.y() + 1.0);
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
 
 int main()
 {
-    const int image_width = 256;
-    const int image_height = 256;
-    
+    const double aspect_ratio = 16.0 / 9.0;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    double viewport_height = 2.0;
+    double view_port_width = aspect_ratio * viewport_height;
+    double focal_length = 1.0;
+
+    point3 origin = point3(0, 0, 0);
+    vec3 horizontal = vec3(view_port_width, 0, 0);
+    vec3 vertical = vec3(0, viewport_height, 0);
+
+    vec3 lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - vec3(0, 0, focal_length);
+
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int row = image_height - 1; row >= 0; --row) {
 
         std::cerr << "\nScanlines remaining: " << row + 1 << ' ' << std::flush;
         for (int col = 0; col < image_width; ++col) {
-            double r = double(col) / ((double)image_width - 1.0);
-            double g = double(row) / ((double)image_height - 1.0);
-            double b = 0.25;
+            double u = double(col) / ((double)image_width - 1.0);
+            double v = double(row) / ((double)image_height - 1.0);
 
-            color pixel_color(r, g, b);
+            ray r(origin,  lower_left_corner + u * horizontal + v * vertical);
+            color pixel_color = ray_color(r);
             write_color(std::cout, pixel_color);
         }
     }
