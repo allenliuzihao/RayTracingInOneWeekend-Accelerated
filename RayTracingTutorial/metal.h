9112b969a7c7955ff2e4a455ae540f02aa3f309e
allenliuzihao <allenliuzihao@gmail.com>
2020-09-28 16:18:47 -0700

    fuzziness

--- RayTracingTutorial/metal.h
+++ RayTracingTutorial/metal.h
@@ -6,15 +6,16 @@
 
 class metal : public material {
 public:
-	metal(const color& a) : albedo(a) {}
+	metal(const color& a, double f) : albedo(a), fuzz(f < 1 ? f : 1) {}
 
 	virtual bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override {
 		vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
 		attenuation = albedo;
-		scattered = ray(rec.p, reflected);
-		return dot(reflected, rec.normal) > 0;
+		scattered = ray(rec.p, reflected + fuzz * random_unit_vector());
+		return dot(scattered.direction(), rec.normal) > 0;
 	}
 
 private:
 	color albedo;
+	double fuzz;
 };
