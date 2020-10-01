8038b593b3e2a2562fd92c67a37c747186e17cda
allenliuzihao <allenliuzihao@gmail.com>
2020-09-30 23:07:20 -0700

    schlick approximation

--- RayTracingTutorial/dialectric.h
+++ RayTracingTutorial/dialectric.h
@@ -14,17 +14,24 @@ public:
         attenuation = color(1.0, 1.0, 1.0);
         double etai_over_etat = rec.front_face ? (1.0 / ref_idx) : ref_idx;
         vec3 unit_direction = unit_vector(r_in.direction());
-        vec3 refracted = refract(unit_direction, rec.normal, etai_over_etat);
-        scattered = ray(rec.p, refracted);
 
-        /*
-        std::cerr << "front face: " << rec.front_face << std::endl;
-        std::cerr << "dir dot normal: " << dot(unit_direction, rec.normal) << std::endl;
-        std::cerr << "unit dot refracted: " << dot(unit_direction, refracted) << std::endl;
-        std::cerr << "refracted: " << refracted.x() << " " << refracted.y() << " " << refracted.z() << std::endl;
+        double cos_theta = fmax(0.0, fmin(1.0,  dot(-unit_direction, rec.normal)));
+        double sin_theta = std::sqrt(1.0 - cos_theta * cos_theta);
 
-        */
-        
+        if (etai_over_etat * sin_theta > 1.0) {
+            vec3 reflected = reflect(unit_direction, rec.normal);
+            scattered = ray(rec.p, reflected);
+        } else {
+            double reflect_prob = schlick(cos_theta, etai_over_etat);
+            if (random_double() < reflect_prob) {
+                vec3 reflected = reflect(unit_direction, rec.normal);
+                scattered = ray(rec.p, reflected);
+                return true;
+            }
+
+            vec3 refracted = refract(unit_direction, rec.normal, etai_over_etat);
+            scattered = ray(rec.p, refracted);
+        }
 
         return true;
     }
--- RayTracingTutorial/material.h
+++ RayTracingTutorial/material.h
@@ -7,3 +7,9 @@ class material {
 public:
 	virtual bool scatter(const ray& r_in, const hit_record& rec, color & attenuation, ray& scattered) const = 0;
 };
+
+double schlick(double cosine, double ref_index) {
+    double r0 = (1 - ref_index) / (1 + ref_index);
+    r0 = r0 * r0;
+    return r0 + (1 - r0) * pow((1 - cosine), 5);
+}
