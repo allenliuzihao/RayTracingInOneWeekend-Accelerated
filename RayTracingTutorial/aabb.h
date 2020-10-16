cfe93ca326ec2edd76d7928e3466fef72c1c2228
allenliuzihao <allenliuzihao@gmail.com>
2020-10-15 23:44:20 -0700

    add optimized aabb

--- RayTracingTutorial/aabb.h
+++ RayTracingTutorial/aabb.h
@@ -31,3 +31,24 @@ private:
 	point3 _min;
 	point3 _max;
 };
+
+inline bool aabb::hit(const ray& r, double tmin, double tmax) const {
+	for (int a = 0; a < 3; a++) {
+		auto invD = 1.0f / r.direction()[a];
+		
+		auto t0 = (min()[a] - r.origin()[a]) * invD;
+		auto t1 = (max()[a] - r.origin()[a]) * invD;
+		
+		if (invD < 0.0f) {
+			std::swap(t0, t1);
+		}
+		
+		tmin = t0 > tmin ? t0 : tmin;
+		tmax = t1 < tmax ? t1 : tmax;
+		
+		if (tmax <= tmin) {
+			return false;
+		}
+	}
+	return true;
+}
